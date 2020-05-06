763167ea7ed6babfda7bee774b41ddedd7c1ea03
peterpaN1k <peetmj@gmail.com>
2020-05-06 14:26:33 +0200

    Updates

---
+++ precache-manifest.d6ebfc9a55007cbedb570cb0886776af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b71689a52c93f25716acf06136ace3",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "1a19b1e598f26f8dd113",
+    "url": "/kanban/static/css/main.33c1f177.chunk.css"
+  },
+  {
+    "revision": "9702f16d2ffe423b1a98",
+    "url": "/kanban/static/js/2.3e4d99e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban/static/js/2.3e4d99e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a19b1e598f26f8dd113",
+    "url": "/kanban/static/js/main.cde7e8c8.chunk.js"
+  },
+  {
+    "revision": "b12bd9cf68aa86a0b2b5",
+    "url": "/kanban/static/js/runtime-main.c595a866.js"
+  }
+]);
